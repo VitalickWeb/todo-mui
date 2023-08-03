9a4c1536cae51b146800763972a305650f188b27
Vitalick <miraigeweb@gmail.com>
2023-08-03 15:14:18 +0300

    add to the project snapshot test

--- src/stories/EditableSpan.stories.tsx
+++ src/stories/EditableSpan.stories.tsx
@@ -1,8 +1,7 @@
 import type {Meta, StoryObj} from '@storybook/react';
-import React, {ChangeEvent, useState} from "react";
+import React from "react";
 import {action} from "@storybook/addon-actions";
-import {EditableSpan, EditableSpanPropsType} from "../Components/EditableSpan";
-import {TextField} from "@material-ui/core";
+import {EditableSpan} from "../Components/EditableSpan";
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta: Meta<typeof EditableSpan> = {
@@ -29,46 +28,46 @@ export const EditableSpanStory: Story = {
     args: {},
 };
 
-const EditableSpanChangeTitleStory = ({
-    title, changeTitle
-}: EditableSpanPropsType) => {
-
-    const [editeMode, setEditMode] = useState<string | boolean>('not change value title')
-    const [localTitle, setLocalTitle] = useState<string>(title)
-
-    const changeAddTaskHandler = (e: ChangeEvent<HTMLInputElement>) => {
-        let changeInputValue = e.currentTarget.value
-        setLocalTitle(changeInputValue)
-        changeTitle(localTitle)
-    }
-
-    const onEditMod = () => {
-        setEditMode(true)
-
-    }
-
-    const offEditMode = () => {
-        changeTitle(localTitle)
-        if (localTitle.length !== 0) {
-            setEditMode(false)
-        }
-    }
-
-    return (
-        editeMode//включен режим редактирования или нет
-            ? <TextField
-                id="Standard secondary"
-                label="Standard secondary"
-                color="secondary"
-                value={localTitle}
-                onChange={changeAddTaskHandler}
-                onBlur={offEditMode}
-                size={'small'}
-            />
-            : <span onDoubleClick={onEditMod}>{title}</span>//если не включен то спан
-    )
-}
-
-export const RewriteTitleStory = () => (
-    <EditableSpanChangeTitleStory changeTitle={action('rewrite title')} />
-);
+// const EditableSpanChangeTitleStory = ({
+//     title, changeTitle
+// }: EditableSpanPropsType) => {
+//
+//     const [editeMode, setEditMode] = useState<string | boolean>('not change value title')
+//     const [localTitle, setLocalTitle] = useState<string>(title)
+//
+//     const changeAddTaskHandler = (e: ChangeEvent<HTMLInputElement>) => {
+//         let changeInputValue = e.currentTarget.value
+//         setLocalTitle(changeInputValue)
+//         changeTitle(localTitle)
+//     }
+//
+//     const onEditMod = () => {
+//         setEditMode(true)
+//
+//     }
+//
+//     const offEditMode = () => {
+//         changeTitle(localTitle)
+//         if (localTitle.length !== 0) {
+//             setEditMode(false)
+//         }
+//     }
+//
+//     return (
+//         editeMode//включен режим редактирования или нет
+//             ? <TextField
+//                 id="Standard secondary"
+//                 label="Standard secondary"
+//                 color="secondary"
+//                 value={localTitle}
+//                 onChange={changeAddTaskHandler}
+//                 onBlur={offEditMode}
+//                 size={'small'}
+//             />
+//             : <span onDoubleClick={onEditMod}>{title}</span>//если не включен то спан
+//     )
+// }
+//
+// export const RewriteTitleStory = () => (
+//     <EditableSpanChangeTitleStory changeTitle={action('rewrite title')} />
+// );
